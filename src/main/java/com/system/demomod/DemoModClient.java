9a1518e6ae4c265c8a6eb197c6432dc8184102fb
sympsel <sympsel@outlook.com>
2025-06-29 23:04:59 +0800

    模组开发入门环境搭建

---
+++ src/main/java/com/system/demomod/DemoModClient.java
@@ -0,0 +1,10 @@
+package com.system.demomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/system/demomod/DemoModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.system.demomod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DemoModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
